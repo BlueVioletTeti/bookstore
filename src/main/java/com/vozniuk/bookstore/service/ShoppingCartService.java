726d72ccc884175714728211c2208f3df0c785d3
Tetiana <tetiana.kolomiiets.fop@gmail.com>
2023-10-12 18:15:30 +0300

    handled lazy initialization problem and added annotation Transactional where it was needed

--- src/main/java/com/vozniuk/bookstore/service/ShoppingCartService.java
+++ src/main/java/com/vozniuk/bookstore/service/ShoppingCartService.java
@@ -1,5 +1,6 @@
 package com.vozniuk.bookstore.service;
 
+import com.vozniuk.bookstore.dto.cart.CartItemDto;
 import com.vozniuk.bookstore.dto.cart.ShoppingCartRequestDto;
 import com.vozniuk.bookstore.dto.cart.ShoppingCartRequestDtoWithoutCartItemId;
 import com.vozniuk.bookstore.dto.cart.ShoppingCartResponseDto;
@@ -9,8 +10,8 @@ public interface ShoppingCartService {
 
     ShoppingCartResponseDto addToCart(ShoppingCartRequestDto requestDto);
 
-    ShoppingCartResponseDto updateCartItem(Long cartItemId,
-                                           ShoppingCartRequestDtoWithoutCartItemId requestDto);
+    CartItemDto updateCartItem(Long cartItemId,
+                               ShoppingCartRequestDtoWithoutCartItemId requestDto);
 
     ShoppingCartResponseDto removeCartItem(Long cartItemId);
 }
