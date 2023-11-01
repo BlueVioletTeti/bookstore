3ad71e0dd0fd974f26d68edb4b0682d69ec18bc4
Tetiana <tetiana.kolomiiets.fop@gmail.com>
2023-11-01 10:35:34 +0200

    deleted unnecessary comments

--- src/test/java/com/vozniuk/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/vozniuk/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,7 @@
 package com.vozniuk.bookstore;
 
-//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookstoreApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-
 }
